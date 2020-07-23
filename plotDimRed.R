448fa4c141152543873bdbfef17e0e6461c47d8f
dalingard <68376712+dalingard@users.noreply.github.com>
2020-07-23 15:39:08 +0100

    Added more colouring options

--- plotDimRed.R
+++ plotDimRed.R
@@ -1,6 +1,10 @@
-reduceDimentions<-function(cells, hvgs, type="UMAP", colourby="cell_type"){
-  if (colourby=="-"){
-    colourby <- 'cell_type'
+reduceDimentions<-function(cells, hvgs, geneOfInterest, type="UMAP", colourby="cell_type", norm_type="batch_corrected"){
+  if (colourby=="Gene expression"){
+    colourby <- geneOfInterest
+  } else if (colourby=="Cell type"){
+    colourby <- "cell_type"
+  } else if (colourby=="Batch") {
+    colourby <- "batch"
   }
   set.seed(100)
   cells <- runPCA(cells, exprs_values="batch_corrected", subset_row=hvgs, ncomponents=25)
@@ -11,13 +15,13 @@ reduceDimentions<-function(cells, hvgs, type="UMAP", colourby="cell_type"){
     colLabels(cells) <- factor(igraph::cluster_louvain(g)$membership)
     
     # Visualization.
-    plotUMAP(cells, colour_by=colourby, by_exprs_values="batch_corrected")
+    plotUMAP(cells, colour_by=colourby, by_exprs_values=norm_type, point_alpha=1, point_size=3)
   } else {
     # Clustering
     g <- buildSNNGraph(cells, use.dimred = 'PCA', k=28)
     colLabels(cells) <- factor(igraph::cluster_louvain(g)$membership)
     
     # Visualization.
-    plotPCA(cells, colour_by=colourby, by_exprs_values="batch_corrected")
+    plotPCA(cells, colour_by=colourby, by_exprs_values=norm_type, point_alpha=1, point_size=3)
   }
 }
