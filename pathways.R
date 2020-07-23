48ed387babed62b7f8eabba2618d7fa9550ea76a
dalingard <68376712+dalingard@users.noreply.github.com>
2020-07-23 15:41:37 +0100

    Pathways now functions with server

--- pathways.R
+++ pathways.R
@@ -1,9 +1,17 @@
 # Expression pathways
 library(pathview)
 
-paths<-function(cellType,colour,pathway,avg)
+paths<-function(cellType, pathway, sce, normalistion = "batch_corrected", avg="median")
 {
-  cells <- assay(sce)[,!colData(sce)$cell_type!=cellType]
+  pathway <- substr(pathway, 4, 8)
+  if (cellType=="Epiblast"){
+    colour <- "#255836"
+  } else if (cellType=="Trophectoderm"){
+    colour <- "#1e4e78"
+  } else {
+    colour <- "#a3202b"
+  }
+  cells <- assay(sce, normalistion)[,!colData(sce)$cell_type!=cellType]
   if (avg=="mean"){
     gene.data <- rowMeans(cells)
   } else {
@@ -12,23 +20,9 @@ paths<-function(cellType,colour,pathway,avg)
   }
   pv.out <- pathview(gene.data = gene.data, pathway.id = pathway, gene.idtype = "symbol", limit=ceiling(max(gene.data)), both.dirs = F, high = colour, mid = "white",
                      out.suffix = paste(avg,".",cellType, sep=""), node.sum = "max")
-  #pv.out$plot.data.gene
+  return(pv.out)
 }
 
 pathways<-list("04010","04012","04014","04015","04020","04022","04024","04064"
                ,"04066","04068","04071","04072","04150","04151","04152","04310"
                ,"04330","04340","04350","04370","04371","04390","04630","04668")
-
-#single pathway
-p <- "04010"
-paths("Epiblast", "#255836", p, "mean")
-paths("Trophectoderm", "#1e4e78", p, "mean")
-paths("Primitive endoderm", "#a3202b", p, "mean")
-
-
-#all pathways
-for (p in pathways){
-  paths("Epiblast", "#255836", p, "median")
-  paths("Trophectoderm", "#1e4e78", p, "median")
-  paths("Primitive endoderm", "#a3202b", p, "median")
-}
