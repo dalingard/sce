de02652d8cc77eb11eed6b8aecf833c637c6d242
dalingard <68376712+dalingard@users.noreply.github.com>
2020-07-27 14:25:46 +0100

    Added selecting Norm type

--- genBoxPlots.R
+++ genBoxPlots.R
@@ -6,13 +6,13 @@ generateBoxPlots <- function(toPlot, norm="batch_corrected"){
       for (g in seq_along(rownames(toPlot))){
         tidyData <- as.data.frame(assay(toPlot, norm))[rownames(toPlot)[[g]],]
         tidyData <- cbind(t(tidyData), cell_type=colData(toPlot)$cell_type)
-        colnames(tidyData) <- c(norm,"cell_type")
+        colnames(tidyData) <- c("norm","cell_type")
         tidyData <- as.data.frame(tidyData)
         tidyData$cell_type <- factor(tidyData$cell_type)
-        tidyData$batch_corrected <- as.numeric(tidyData$batch_corrected)
+        tidyData$norm <- as.numeric(tidyData$norm)
         
         p[[g]] <- expr %>%
-          ggplot(data=tidyData, mapping=aes(cell_type, batch_corrected, group=cell_type, fill=cell_type)) +
+          ggplot(data=tidyData, mapping=aes(cell_type, norm, group=cell_type, fill=cell_type)) +
           geom_boxplot(outlier.colour = "red", outlier.shape = 8) +
           scale_fill_manual(values = col_pal) +
           labs(x = "", y = norm, title = rownames(toPlot)[[g]]) + theme_bw() +
