0f31148315c37b7c6dd52597be326011adee0241
dalingard <68376712+dalingard@users.noreply.github.com>
2020-07-27 14:14:03 +0100

    added generating box plots

---
+++ genBoxPlots.R
@@ -0,0 +1,29 @@
+generateBoxPlots <- function(toPlot, norm="batch_corrected"){
+  col_pal <- c("#4daf4a", "#e41a1c", "#377eb8")
+  if(!is.null(toPlot)){
+    withProgress({
+      p <- list()
+      for (g in seq_along(rownames(toPlot))){
+        tidyData <- as.data.frame(assay(toPlot, norm))[rownames(toPlot)[[g]],]
+        tidyData <- cbind(t(tidyData), cell_type=colData(toPlot)$cell_type)
+        colnames(tidyData) <- c(norm,"cell_type")
+        tidyData <- as.data.frame(tidyData)
+        tidyData$cell_type <- factor(tidyData$cell_type)
+        tidyData$batch_corrected <- as.numeric(tidyData$batch_corrected)
+        
+        p[[g]] <- expr %>%
+          ggplot(data=tidyData, mapping=aes(cell_type, batch_corrected, group=cell_type, fill=cell_type)) +
+          geom_boxplot(outlier.colour = "red", outlier.shape = 8) +
+          scale_fill_manual(values = col_pal) +
+          labs(x = "", y = norm, title = rownames(toPlot)[[g]]) + theme_bw() +
+          theme(legend.position = "none") +
+          scale_y_continuous(breaks = scales::pretty_breaks(n = 10))
+        
+        incProgress()
+      }
+      return(plot_grid(plotlist = p, ncol = 2))
+    }, message = "Making boxplots...")
+  }else{
+    return(NULL)
+  }
+}
