ee73fa7b55a10044fe65a3b043305e8c6c1d3425
dalingard <68376712+dalingard@users.noreply.github.com>
2020-07-28 16:35:22 +0100

    Fixed scaling and added bp highlight

--- server.R
+++ server.R
@@ -13,7 +13,6 @@ library(ggplot2)
 library(cowplot)
 library(scater)
 library(scran)
-library(png)
 
 load("sce_batchCorrected.RData")
 source('plotDimRed.R')
@@ -23,6 +22,35 @@ source('genBoxPlots.R')
 plotHeight <- function(numPlots){
   prows <- ceiling(numPlots/2)
   return(prows*250)
+}
+
+getGenes <- function(input, values){
+  values$toHighlight <- NULL
+  group <- filter(values$pathdata[[1]], input$pathway_click$coords_img$x>=x-(0.5*width))
+  group <- filter(group, input$pathway_click$coords_img$x<=(x)+(0.5*width))
+  group <- filter(group, input$pathway_click$coords_img$y>=y-(0.5*height))
+  group <- filter(group, input$pathway_click$coords_img$y<=(y)+(0.5*height))
+  values$selectedGroup <- group
+  return(values)
+}
+
+getCellsToPlot <- function(input, values){
+  entrezCells <- sce[!is.na(rowData(sce)$entrez), ]
+  group <- unlist(strsplit(values$selectedGroup$all.mapped, ","))
+  group <- entrezCells[rowData(entrezCells)$entrez %in% group,]
+  values$toPlot <- group
+  return(values)
+}
+  
+getGeneToHighlight <- function(input, values){
+  if (length(rownames(values$toPlot))>1){
+    cells <- assay(values$toPlot, input$pathwayNtype)[,!colData(values$toPlot)$cell_type!=input$ctype]
+    gene.data <- rowMedians(cells)
+    names(gene.data) <- rownames(cells)
+    gene.data <- sort(gene.data, TRUE)
+    values$toHighlight <- names(gene.data)[[1]]
+    return(values)
+  }
 }
 
 # Define server logic
@@ -35,9 +63,10 @@ shinyServer(function(input, output, session) {
   values <- reactiveValues(
     pathdata = NULL,
     normalisation = NULL,
-    xsf = 1,
-    ysf = 1,
-    toPlot = NULL
+    selectedGroup = NULL,
+    toPlot = NULL,
+    genesToPlot = NULL,
+    toHighlight = NULL
   )
   
   #deactivate/activate inputs
@@ -51,23 +80,14 @@ shinyServer(function(input, output, session) {
     }
   })
   
-  #on click of pathway get gene data to plot
-  observeEvent(input$pathway_click,{
-    values$xsf <- 1
-    group <- filter(values$pathdata[[1]], input$pathway_click$x>=x*values$xsf-(0.5*width*values$xsf))
-    group <- filter(group, input$pathway_click$x<=(x*values$xsf)+(0.5*width*values$xsf))
-    group <- filter(group, input$pathway_click$y>=y*values$ysf-(0.5*height*values$ysf))
-    group <- filter(group, input$pathway_click$y<=(y*values$ysf)+(0.5*height*values$ysf))
-    entrezCells <- sce[!is.na(rowData(sce)$entrez), ]
-    group <- unlist(strsplit(group$all.mapped, ","))
-    group <- entrezCells[rowData(entrezCells)$entrez %in% group,]
-    values$toPlot <- group
-  })
   
+  #on click of pathway get gene data to plot
+  observeEvent(input$pathway_click, {
+    values <- getGenes(input, values)
+  })
   
   #plot pathway
   output$pathway <- renderImage({
-    
     width  <- session$clientData$output_pathway_width*0.9
     height <- session$clientData$output_pathway_height
     pixelratio <- session$clientData$pixelratio
@@ -76,12 +96,8 @@ shinyServer(function(input, output, session) {
     values$pathdata <- data
     
     filename <- normalizePath(file.path('./', paste(substr(input$spath,1,8), '.median.', input$ctype, '.png', sep='')))
-    img <- readPNG(filename)
-    size <- dim(img)
-    values$xsf <- width/size[[1]]
-    # Return a list containing the filename and alt text
     list(src = filename,
-         #width = width,
+         width = width,
          alt = "Pathway")
   }, deleteFile = FALSE)
   
@@ -95,27 +111,31 @@ shinyServer(function(input, output, session) {
   
   #plot box plots on pathway tab
   output$expr_boxplot <- renderPlot({
-    generateBoxPlots(values$toPlot, input$pathwayNtype)
+    generateBoxPlots(values$toPlot, input$pathwayNtype, values$toHighlight)
   })
   
   #ui wrapper for boxplots on pathway tab
   output$ui_plot <- renderUI({
-    numPlots <- length(rownames(values$toPlot))
-    if (numPlots > 0){
-      plotOutput("expr_boxplot", height=plotHeight(numPlots))
+    if (!is.null(values$selectedGroup)){
+      numPlots <- length(unlist(strsplit(values$selectedGroup$all.mapped,",")))
+      if (numPlots > 0){
+        values <- getCellsToPlot(input, values)
+        values <- getGeneToHighlight(input, values)
+        plotOutput("expr_boxplot", height=plotHeight(numPlots))
+      }
     }
   })
   
   #plot box plots on boxplots tab
   output$g_boxplot <- renderPlot({
-    generateBoxPlots(values$toPlot, input$bp_ntype)
+    generateBoxPlots(values$genesToPlot, input$bp_ntype)
   })
   
   #ui wrapper for boxplots on boxplots tab
   output$bp_ui_plot <- renderUI({
     numPlots <- length(input$bp_goi)
     if (numPlots > 0){
-      values$toPlot <- sce[rowData(sce)$symbol %in% input$bp_goi,]
+      values$genesToPlot <- sce[rowData(sce)$symbol %in% input$bp_goi,]
       plotOutput("g_boxplot", height=plotHeight(numPlots))
     }
   })
