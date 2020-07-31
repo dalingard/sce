4bae398d48f84e656f1e97827a3b86c3cc9a5704
dalingard <68376712+dalingard@users.noreply.github.com>
2020-07-31 17:39:26 +0100

    Added support for cluster colouring

--- plotDimRed.R
+++ plotDimRed.R
@@ -1,10 +1,12 @@
 reduceDimentions<-function(cells, hvgs, geneOfInterest, type="UMAP", colourby="cell_type", norm_type="batch_corrected"){
   if (colourby=="Gene expression"){
     colourby <- geneOfInterest
-  } else if (colourby=="Cell type"){
+  } else if (colourby=="Cell type" || colourby=="Cell-type based"){
     colourby <- "cell_type"
   } else if (colourby=="Batch") {
     colourby <- "batch"
+  } else if (colourby=="Cluster-based"){
+    colourby <- "label"
   }
   
   if (norm_type=="fpkm" || norm_type=="tpm"){
